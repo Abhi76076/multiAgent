fc42fa8bd750779092033454d79cf7f9d0378296
Abhi76076 <abhishek7607691@gmail.com>
2026-04-10 23:07:40 +0530

    Delete scratch.py

--- scratch.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-from dotenv import load_dotenv
-
-load_dotenv()
-client = genai.Client()
-
-for m in client.models.list_models():
-    if "flash" in m.name:
-        print(m.name)
